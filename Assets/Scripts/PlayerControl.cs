b1f1934d51616e2cf36d33cb4fa4c18c0829bed3
Kuba7eichen <kuba.siebeneichen@gmail.com>
2022-11-30 14:40:52 +0100

    Firing gifts works

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private GameObject gift;
+    [SerializeField] private Camera gameCamera;
+    [SerializeField] private float giftForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            FireGift();
+    }
+
+    void FireGift()
+    {
+        GameObject newGift = Instantiate(gift, transform.position + gameCamera.transform.forward, new Quaternion());
+        newGift.GetComponent<Rigidbody>().AddForce(gameCamera.transform.forward * giftForce, ForceMode.Impulse);
+    }
+}
